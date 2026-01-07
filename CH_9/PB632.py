0e550aef7b1baa6756071ac6b0b25ce6675158b6
sohanipayla <sohanipayla@gmail.com>
2026-01-07 09:42:20 +0530

    Update PB632.py

--- CH_9/PB632.py
+++ CH_9/PB632.py
@@ -7,14 +7,9 @@ class Book:
         self.isbn = isbn
         self.year = year
     def display(self):
-         def print_data(self):
-        print("\n\n")
-        print(f"Name: {self.name}")
-        print(f"Number of authors: {self.n}")
-        print(f"Authors: {self.authors}")
-        print(f"Publisher: {self.publisher}")
-        print(f"ISBN: {self.ISBN}")
-        print(f"Year: {self.year}")
+        print(f"Book Name: {self.name}")
+        print(f"Authors ({self.n}): {', '.join(self.authors)}")
+        print(f"Publisher: {self.publisher} | ISBN: {self.isbn} | Year: {self.year}")
 
 class CourseBook(Book):
     def __init__(self, name, authors, publisher, isbn, year, course):
@@ -28,3 +23,4 @@ auth_list = ["H.M. Deitel", "P.J. Deitel"]
 my_book = CourseBook("Python Programming", auth_list, "Pearson", "978-0134034287", 2024, "Computer Science")
 
 my_book.display()
+
